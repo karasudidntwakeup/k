ff35e8473d60b7ce5dfba7a8ccebe657f3bd1160
karasu <31324615+karasudidntwakeup@users.noreply.github.com>
2022-07-25 12:37:56 +0200

    colors

---
+++ dwm/st/colors-wal-st.h
@@ -0,0 +1,30 @@
+const char *colorname[] = {
+
+  /* 8 normal colors */
+  [0] = "#ffffff", /* black   */
+  [1] = "#9688e2", /* red     */
+  [2] = "#b9fdcd", /* green   */
+  [3] = "#fab387", /* yellow  */
+  [4] = "#9688e2", /* blue    */
+  [5] = "#ffc0cb", /* magenta */
+  [6] = "#0a0d0f", /* cyan    */
+  [7] = "#0f1214", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#ffffff",  /* black   */
+  [9]  = "#4B3655",  /* red     */
+  [10] = "#453B6B", /* green   */
+  [11] = "#ffffc6", /* yellow  */
+  [12] = "#c4fffe", /* blue    */
+  [13] = "#d7caed", /* magenta */
+  [14] = "#FFFFFF", /* cyan    */
+  [15] = "#fab387", /* white   */
+
+  /* special colors */
+  [256] = "#0F0D1C", /* background */
+  [257] = "#FFFFFF", /* foreground */
+  [258] = "#FFFFFF",     /* cursor */
+};
+
+/* Default colors (colorname index)
+ * foreground, background, cursor */
