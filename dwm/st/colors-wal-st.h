da3d82cab1ca12c02dedc288b9cc18e49e86937f
karasu <31324615+karasudidntwakeup@users.noreply.github.com>
2022-07-27 12:49:28 +0200

    Add files via upload

--- dwm/st/colors-wal-st.h
+++ dwm/st/colors-wal-st.h
@@ -1,17 +1,17 @@
 const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#ffffff", /* black   */
-  [1] = "#9688e2", /* red     */
+  [0] = "#282a2e", /* black   */
+  [1] = "#a54242", /* red     */
   [2] = "#b9fdcd", /* green   */
   [3] = "#fab387", /* yellow  */
-  [4] = "#9688e2", /* blue    */
+  [4] = "#a987de", /* blue    */
   [5] = "#ffc0cb", /* magenta */
   [6] = "#0a0d0f", /* cyan    */
-  [7] = "#0f1214", /* white   */
+  [7] = "#FFFFFF", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#ffffff",  /* black   */
+  [8]  = "#1e1a37",  /* black   */
   [9]  = "#4B3655",  /* red     */
   [10] = "#453B6B", /* green   */
   [11] = "#ffffc6", /* yellow  */
